518d3b01d35cb5f2199c962e1c08d0e6a9157829
tkbtk-kic <s16002@st.kic.ac.jp>
2016-11-03 12:33:09 +0900

    change main.rb, add webrick/html

--- WebrickServer/ruby/httpserver.rb
+++
@@ -1,8 +0,0 @@
-require 'sinatra'
-
-s=HTTPServer.new(
-	:port => 8000,
-	:DocumentRoot => File.join(Dir::pwd, "html")
-)
-trap("INT"){s.shutdown}
-s.start
--- testtwitter.php
+++ testtwitter.php
@@ -2,11 +2,6 @@
 require "twitteroauth/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-$consumerKey = "uyrXf2COfXQNiyqfAwX21FwF9";
-$consumerSecret ="gi8GfznUm3Kr4IuYKVORhX2G3wfVg2WDWBoHy6Bn3yNyLpmUuU";
-$accessToken = "2497747381-XiSKaT51LSqimfwv9zGBYS30ypq0VTfUROOhAtl";
-$accessTokenSecret ="Vx5kC2bXDvLVMUKpx8Sn4V1S3uue5hXVM5uDaPaz3aIoE";
-
 $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 //var_dump($connection);
 
