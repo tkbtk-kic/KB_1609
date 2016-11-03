6a0adaf6a2653f606d3aeadc6c5c677b0627a604
tkbtk-kic <s16002@st.kic.ac.jp>
2016-11-03 14:10:06 +0900

    #2 応急処置としてファイル読み込みにした

--- testtwitter.php
+++ testtwitter.php
@@ -2,8 +2,22 @@
 require "twitteroauth/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 
+$jsonUrl = "Twitterkey.json"; //JSONファイルの場所とファイル名を記述
+if(file_exists($jsonUrl)){
+  $json = file_get_contents($jsonUrl);
+  $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+  $obj = json_decode($json,true);
+  //var_dump($obj);
+  $consumerKey = $obj["CONSUMERKEY"];
+  $consumerSecret = $obj["CONSUMERSECRET"];
+  $accessToken = $obj["ACCESSTOKEN"];
+  $accessTokenSecret = $obj["ACCESSTOKENSECRET"];
+}
+
+//var_dump($consumerKey);
+
 $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
-//var_dump($connection);
+var_dump($connection);
 
 $tweets_params =["geocode" => "34.694343,135.194507,25km"];
 
