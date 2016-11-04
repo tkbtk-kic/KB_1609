e4bed671adf92c5c11a3adafcbd3f4036e2155a1
tkbtk-kic <s16002@st.kic.ac.jp>
2016-11-04 23:57:19 +0900

    post json and json encord to Hash

--- testtwitter.php
+++ testtwitter.php
@@ -9,7 +9,8 @@ if(file_exists($jsonUrl)){
   $obj = json_decode($json,true);
   //var_dump($obj);
   $consumerKey = $obj["CONSUMERKEY"];
-  $consumerSecret = $obj["CONSUMERSECRET"];
+  $consumerSecret = $obj["CONSUMERSEC
+  RET"];
   $accessToken = $obj["ACCESSTOKEN"];
   $accessTokenSecret = $obj["ACCESSTOKENSECRET"];
 }
@@ -17,14 +18,43 @@ if(file_exists($jsonUrl)){
 //var_dump($consumerKey);
 
 $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
-var_dump($connection);
+//var_dump($connection);
 
 $tweets_params =["geocode" => "34.694343,135.194507,1km"];
 
 $tweets = $connection->get('search/tweets', $tweets_params);
+// この時点で$tweetsはオブジェクト型らしい
 //var_dump($tweets);
 $arr = json_encode($tweets);
-file_put_contents("geosearch.json" , $arr);
+//file_put_contents("geosearch.json" , $arr);
+
+$url = 'http://localhost:4567/edit';
+$headers = ['Content-type: application/json'];
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $arr);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($curl);
+curl_close($curl);
+
+echo $response, PHP_EOL;
+
+//$post=[];
+//foreach($tweets as $key => $value){
+//    if($key == "statuses"){
+//        $i=0;
+//        print ("status");
+//        foreach($value as $key => $value){
+//            var_dump($value);
+//            foreach($value as $key => $value){
+//                var_dump($key);
+//            }
+//        }
+//    }
+//    var_dump($key);
+//    var_dump($value);
+//}
 
 ?>
 
