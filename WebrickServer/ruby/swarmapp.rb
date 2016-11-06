ec012e8a4e80dab2ab6997f44c1809833ee4ccce
tkbtk-kic <s16002@st.kic.ac.jp>
2016-11-06 12:39:58 +0900

    remove Unnecessary files

--- WebrickServer/ruby/swarmapp.rb
+++
@@ -1,9 +0,0 @@
-require 'json'
-
-json_file_path = 'one_tweet.json'
-
-json_data = open(json_file_path) do |io|
-  JSON.load(io)
-end
-
-p json_data["entities"]["urls"][0]["display_url"][0, 12] == "swarmapp.com"
